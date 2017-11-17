f6baf4c2d76a0d46898daa60419880bffc589c0d
Donray Williams <w.donray@yahoo.com>
2017-11-17 10:49:40 -0600

    what

--- Assets/Scripts/Boids/GameController.cs
+++ Assets/Scripts/Boids/GameController.cs
@@ -26,21 +26,22 @@ namespace BoidsSpace
     public class GameController : MonoBehaviour
     {
         public GameObject agentPrefab;
-        [Range(10, 50)]public int BoidSize;
+        [Range(10, 50)] public int BoidSize;
         public static int Count;
-        public UIController _uiController;
+        private UIController _uiController;
         public static List<AgentBehaviour> AgentBehaviours;
         public static List<Agent> Agents;
         public static bool ToggleAvoidBool;
         public void Start()
         {
+            _uiController = GetComponent<UIController>();
             ToggleAvoidBool = false;
-            BoidSize = 35;
+            BoidSize = 25;
             Count = 0;
             Create();
         }
         public void Update()
-        { 
+        {
             if (Input.GetKeyDown(KeyCode.Escape))
                 Application.Quit();
         }
@@ -48,15 +49,10 @@ namespace BoidsSpace
         [ContextMenu("Create")]
         public void Create()
         {
-            _uiController.MaxSpeedSL.gameObject.SetActive(true);
-            _uiController.DispersionSL.gameObject.SetActive(true);
-            _uiController.CohesionSL.gameObject.SetActive(true);
-            _uiController.AlignmentSL.gameObject.SetActive(true);
             Agents = new List<Agent>();
             AgentBehaviours = new List<AgentBehaviour>();
             for (var i = 0; i < Count; i++)
             {
-                //var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 var go = Instantiate(agentPrefab);
                 go.transform.SetParent(transform);
                 go.name = string.Format("{0} {1}", "Agent: ", i);
@@ -82,10 +78,6 @@ namespace BoidsSpace
             Count = 0;
             Agents.Clear();
             AgentBehaviours.Clear();
-            _uiController.MaxSpeedSL.gameObject.SetActive(false);
-            _uiController.DispersionSL.gameObject.SetActive(false);
-            _uiController.CohesionSL.gameObject.SetActive(false);
-            _uiController.AlignmentSL.gameObject.SetActive(false);
         }
         [ContextMenu("Add")]
         public void Add(int amount)
@@ -105,6 +97,19 @@ namespace BoidsSpace
         {
             ToggleAvoidBool = !ToggleAvoidBool;
             _uiController.AvoidText.text = ToggleAvoidBool == false ? "Avoid Object = Off" : "Avoid Object = On";
+        }
+
+        public void RealRefresh()
+        {
+            foreach (var v in AgentBehaviours)
+            {
+                DestroyImmediate(v.gameObject);
+            }
+            Agents.Clear();
+            AgentBehaviours.Clear();
+            var temp = Count;
+            Count = temp;
+            Create();
         }
         #endregion Helpers
     }
